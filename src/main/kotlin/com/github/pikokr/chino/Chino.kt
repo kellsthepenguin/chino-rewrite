d4d27ea23ebc2da27e65af228dbe9acbdc221b89
pikokr <parangee9706@gmail.com>
2020-11-03 22:47:38 +0900

    Feat: init JDA

--- src/main/kotlin/com/github/pikokr/chino/Chino.kt
+++ src/main/kotlin/com/github/pikokr/chino/Chino.kt
@@ -1,5 +1,12 @@
 package com.github.pikokr.chino
 
+import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.JDABuilder
+
 object Chino {
     val config = ChinoConfigManager()
+    val jda: JDA
+    init {
+        jda = JDABuilder.createDefault(config.token).build()
+    }
 }
