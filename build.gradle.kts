573810229050d7772bd659c68c14a1569d9aae36
pikokr <parangee9706@gmail.com>
2020-11-03 22:44:16 +0900

    Feat: install JDA

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("com.natpryce:konfig:1.6.10.0")
+    implementation("net.dv8tion:JDA:4.2.0_168")
     testCompile("junit", "junit", "4.12")
 }
