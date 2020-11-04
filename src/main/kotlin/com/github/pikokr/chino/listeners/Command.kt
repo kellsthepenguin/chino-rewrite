cd92ccf9c03951581154b10bef5842a883675d41
pikokr <parangee9706@gmail.com>
2020-11-04 18:34:53 +0900

    Feat: make Command class to open

--- src/main/kotlin/com/github/pikokr/chino/listeners/Command.kt
+++ src/main/kotlin/com/github/pikokr/chino/listeners/Command.kt
@@ -1,6 +1,5 @@
 package com.github.pikokr.chino.listeners
 
-class Command(val name: String, val aliases: Array<String> = arrayOf(), val ownerOnly: Boolean = false, val guildOnly: Boolean = false) {
-    open fun execute() {
-    }
+open class Command(val name: String, val aliases: Array<String> = arrayOf(), val ownerOnly: Boolean = false, val guildOnly: Boolean = false) {
+    open fun execute() {}
 }
