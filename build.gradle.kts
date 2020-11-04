0c0a43707711c504af3683f329e80f8f292aa00e
pikokr <parangee9706@gmail.com>
2020-11-04 20:55:04 +0900

    Feat: install kotlinx.coroutines

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1")
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
     implementation("com.natpryce:konfig:1.6.10.0")
