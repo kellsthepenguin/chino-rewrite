7d4c0d367b2a683e22a27569ece4f310a527fc93
pikokr <parangee9706@gmail.com>
2020-11-04 22:14:00 +0900

    Backup

--- src/main/kotlin/com/github/pikokr/chino/structs/ListenerDSL.kt
+++ src/main/kotlin/com/github/pikokr/chino/structs/ListenerDSL.kt
@@ -1,12 +1,16 @@
 package com.github.pikokr.chino.structs
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import net.dv8tion.jda.api.events.GenericEvent
 import kotlin.reflect.KClass
 
 class ListenerDSL(private val event: GenericEvent) {
-    fun <E: GenericEvent> evt(event: KClass<E>, invoke: (E) -> Unit) {
-        if (this.event::class.java === event.java) {
-            invoke(this.event as E)
+    fun <E: GenericEvent> evt(e: KClass<E>, invoke: suspend (E) -> Unit) {
+        if (this.event::class.java === e.java) {
+            GlobalScope.launch {
+                invoke(event as E)
+            }
         }
     }
 }
