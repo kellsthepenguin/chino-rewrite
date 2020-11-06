005e3acb0c5994a759f3be711f256946728e8320
pikokr <parangee9706@gmail.com>
2020-11-06 15:38:30 +0900

    JS를 버리지 못한 파랑이(백엔드 못만들겠다)

--- src/main/kotlin/com/github/pikokr/chino/listeners/Command.kt
+++
@@ -1,8 +0,0 @@
-package com.github.pikokr.chino.listeners
-
-data class CommandInfo(val name: String="", val aliases: ArrayList<String> = arrayListOf(), val ownerOnly: Boolean = false, val guildOnly: Boolean = false, val category: String = "default")
-
-open class Command {
-    open fun getInfo(): CommandInfo = CommandInfo()
-    open suspend fun execute(ctx: CommandContext) {}
-}
--- src/main/kotlin/com/github/pikokr/chino/main.kt
+++
@@ -1,6 +0,0 @@
-package com.github.pikokr.chino
-
-fun main() {
-    println("Starting...")
-    Chino
-}
