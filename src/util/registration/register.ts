4d813ced6d9f8aa7c7f6cb5b9910847c5327348b
pikokr <parangee9706@gmail.com>
2020-11-07 16:53:35 +0900

    Feat: delete register translations

--- src/util/registration/register.ts
+++ src/util/registration/register.ts
@@ -2,8 +2,23 @@ import CommandContext from "../command/CommandContext";
 import {MessageReaction} from "discord.js";
 
 export default async (ctx: CommandContext) => {
+    const lng = {
+        "register": {
+            "title": "Register",
+            "desc": "You should register to use chino's services!\nYou can register with clicking :white_check_mark: reaction!",
+            "success": {
+                "title": "Success!",
+                "desc": "You can use chino's service now.\nIf you are not korean, you can select other languages with {{prefix}}lang command!"
+            },
+            "cancelled": {
+                "title": "Cancelled",
+                "desc": "You cancelled the registration process."
+            }
+        }
+    }
+
     const embed = ctx.embed()
-    embed.setTitle(ctx.t('common:register.title')).setDescription(ctx.t('common:register.desc'))
+    embed.setTitle(lng.register.title).setDescription(lng.register.desc)
     const m = await ctx.chn.send(embed)
     await m.react('✅')
     await m.react('❌')
@@ -12,12 +27,10 @@ export default async (ctx: CommandContext) => {
         max: 1
     })
     if (!collected.size || collected.first()?.emoji.name === '❌') {
-        await m.edit(embed.setTitle(ctx.t('common:register.cancelled.title')).setDescription(ctx.t('common:register.cancelled.desc')))
+        await m.edit(embed.setTitle(lng.register.cancelled.title).setDescription(lng.register.cancelled.desc))
         return false
     }
     await ctx.bot.db('users').insert({id: ctx.author.id})
-    await m.edit(embed.setTitle(ctx.t('common:register.success.title')).setDescription(ctx.t('common:register.success.desc', {
-        prefix: ctx.prefix
-    })))
+    await m.edit(embed.setTitle(lng.register.success.title).setDescription(lng.register.success.desc.replace('{{prefix}}', ctx.prefix)))
     return true
 }
