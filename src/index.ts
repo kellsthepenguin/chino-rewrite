cefa034cae9edb26c26ecdde4e154df9651e804c
Kim Jioo <jwkim101201@gmail.com>
2020-11-27 09:22:27 +0900

    ummmm

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ import {ShardingManager} from 'discord.js'
 import path from "path";
 import config from '../config.json'
 
-console.log(`${figlet.textSync('CHINOBOT', {
+const figletBotName = figlet.textSync('CHINOBOT', {
     font: 'Soft'
-})}
-ChinoBOT is starting...`)
+})
+
+console.log(`${figletBotName}\nChinoBOT is starting...`)
 
 const manager = new ShardingManager(path.join(__dirname, 'shard.ts'), {
     execArgv: ['-r', 'ts-node/register'],
